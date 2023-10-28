1f23e8491ad3ba76c61c15f2380a248b966cab33
Фафанов Роман <148956988+Ballbasabr@users.noreply.github.com>
2023-10-28 03:53:29 +0300

    Update дз1.sql

--- Проект №2/дз1.sql
+++ Проект №2/дз1.sql
@@ -1,14 +1,42 @@
-select user_id, user_email from users
-WHERE user_email = 'houston42@gmail.com';
+/*Найдите id (поле user_id) пользователя с почтой houston42@gmail.com*/
 
-select user_id, level from student
-WHERE user_id = 44133;
+SELECT 
+  user_id, user_email from users
+WHERE 
+  user_email = 'houston42@gmail.com';
 
-select * from subject
-WHERE subject_id = 8;
+/*Найдите уровень (поле level) студента с user_id = 44133*/
 
-select group_id, email from teacher
-WHERE group_id = 80;
+SELECT 
+  user_id, level 
+FROM 
+  student
+WHERE 
+  user_id = 44133;
 
-select DISTINCT education_form from student
-WHERE user_id = 12203;
+/*Найдите предмет (поле subject_title) с id = 8*/
+
+SELECT
+  * 
+FROM 
+  subject
+WHERE 
+  subject_id = 8;
+
+/*Найдите email учителя группы с group_id = 80*/
+
+SELECT 
+  group_id, email 
+FROM
+  teacher
+WHERE 
+  group_id = 80;
+
+/*Выведите уникальные форматы обучения для студента с user_id = 12203*/
+
+SELECT 
+  DISTINCT education_form 
+FROM
+  student
+WHERE 
+  user_id = 12203;
